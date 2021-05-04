8806b1779e3a1f28da45302b452083a46b6338e1
김소은 <soeunkim0409@gmail.com>
2021-05-04 11:07:58 +0900

    반복문 (for,continue,break)

--- JavaEx/src/com/javaex/basics/LoopEx.java
+++ JavaEx/src/com/javaex/basics/LoopEx.java
@@ -5,12 +5,86 @@ import java.util.Scanner;
 public class LoopEx {
 
 	public static void main(String[] args) {
-		whileEx();
+		//whileEx();
 		//whileGugu();
 		//whileInfinity();
-		doWhileEx();
+		//doWhileEx();
+		//forLoopEx();
+		//continueEx();
+		//breakEx();
+		//randomEx();
+		guguEx();
 	}
+	
+	private static void guguEx() {
+	
+		//for (제어 변수 초기화;반복조건 체크;증감식)
+	
+	for (int i=0; i<=6; i++) {
+		System.out.println();
+	}
+		}
 		
+		
+		private static void randomEx() {
+			//1~45까지의 난수 6개 출력 (중복x)
+			//for문 버전
+			for (int i=0; i<6; i++) {
+				int random = (int)(Math.random()* 45) +1 ;
+				System.out.println(random + "");
+			}
+			System.out.println(); //개행 
+			
+			//while문 버전
+			int i=0; 
+			
+			while(i<6) {
+				System.out.print((int)(Math.random() * 45) +1);
+				System.out.print(" ");
+				i++;	
+			}
+			System.out.println();
+			
+		}
+private static void breakEx() {
+	//6과 14로 동시에 나누어 떨어지는 수를 찾아보자
+	//어디까지 루프를 돌려야 할지 판단이 어려움으로 -> while문으로 
+	int num=1;
+	while (true) {
+		if (num %6 ==0 && num%14==0) break;
+		System.out.println(".");
+		num ++; 
+	}
+	System.out.println("\n" +num);
+	}
+
+private static void  continueEx() {
+	//1~20수 중 2의 배수, 3의 배수를 제외한 나머지를 출력 
+		for (int i=1;i<=20;i++) {
+			if(i%2==0 || i%3==0 ) continue;
+			//2의 배수, 3의 배수면 아래 코드가 진행되지 않고 다음 루프를 수행함 
+			System.out.println(i);
+						
+		}
+}
+
+	private static void forLoopEx() {
+		//구구표 출력 (for문 버전 > whileGugu와 비교
+		Scanner scanner= new Scanner (System.in);
+		System.out.print("단을 입력하세요:");
+		int dan = scanner.nextInt();
+		
+		//for (제어 변수 초기화;반복조건 체크;증감식)
+		for(int num=1; num<=9; num++) {
+			System.out.println(dan + "*" + num + "=" + (dan*num));
+		}
+		
+		scanner.close();
+		
+	}
+	
+	
+	
 private static void doWhileEx() {
 	//while문의 반복 조건을 뒤에서 확인
 	//최소 1회는 실행해야 하는 경우 do~while 문 사용
