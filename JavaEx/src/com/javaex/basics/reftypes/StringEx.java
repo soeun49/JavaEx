8b099a9a4e43fffc72cfb5828ede8bca4101caed
김소은 <soeunkim0409@gmail.com>
2021-05-04 13:25:04 +0900

    문자열

--- JavaEx/src/com/javaex/basics/reftypes/StringEx.java
+++ JavaEx/src/com/javaex/basics/reftypes/StringEx.java
@@ -4,9 +4,37 @@ public class StringEx {
 
 	public static void main(String[] args) {
 		stringBasic();
+		stringFormatEx();
 		
 	}
 
+	private static void stringFormatEx() {
+		//형식화된 문자열
+		//%s(문자열), %d(정수), %f(실수), %n(개행), %%(%)
+		//_개의 _중에서 _개를 먹었다 --> 문자열 생성
+		
+		String food = "사과";
+		int total = 10;
+		int eat= 3;
+		System.out.println(total+"개의 "+food +" 중에서 "+ eat +"개를 먹었다."); 
+		
+		// 포맷화된 형식으로 출력
+		System.out.printf("%d개의 %s 중에서 %d개를 먹었다. %n", total, food, eat);
+		
+		//%d, %f 등의 포맷문자에는 부가 정보를 담을 수 있음 
+		float rate=1.234f;
+		//현재 이자율은 몇 %인가?
+		System.out.printf("현재 이자율은%f%%입니다.%n", rate);	
+		//부가 정보로 소숫점 자릿수 지정
+		System.out.printf("현재 이자율은 %.2f%% 입니다.%n", rate);
+		
+		//포맷 형식은 String.format 매서드를 이용, 문자열 생성시 활용 가능
+		String fmt = "%d개의 %s 중에서 %d개를 먹었다%n"; //포맷을 미리 만들고
+		String result= String.format(fmt, 5, "바나나",2); //데이터 연결
+		System.out.println(result); //생성
+				
+	}
+		
 	private static void stringBasic() {
 		//문자열 선언
 		String str; // 선언
