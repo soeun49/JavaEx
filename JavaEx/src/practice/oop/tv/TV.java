da4d087733b5e7d42b95dd898db0ea526eddde94
김소은 <soeunkim0409@gmail.com>
2021-05-07 17:52:04 +0900

    oop 연습문제_TV

---
+++ JavaEx/src/practice/oop/tv/TV.java
@@ -0,0 +1,28 @@
+package practice.oop.tv;
+
+public class TV {
+
+	private int x;
+	private int y;
+	private boolean z;
+	
+	public Point (int x, int y, boolean z) {
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public boolean isZ() {
+		return z;
+	}
+	
+	
+}
---
+++ JavaEx/src/practice/oop/tv/TVApp.java
@@ -0,0 +1,13 @@
+package practice.oop.tv;
+
+public class TVApp {
+
+public static void main(String[] args) {	
+	
+	TV tv= new TV (7,20,false);
+	
+	
+			
+	
+}
+}
