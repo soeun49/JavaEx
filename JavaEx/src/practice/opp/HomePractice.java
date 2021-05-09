dc7367d4a04ca188722f8274fcd9ab6563d4f059
김소은 <soeunkim0409@gmail.com>
2021-05-09 15:31:15 +0900

    연습문제 풀이

---
+++ JavaEx/src/practice/opp/HomePractice.java
@@ -0,0 +1,56 @@
+package practice.opp;
+
+public class HomePractice {
+public static void main (String [] args) {
+	ifEx();
+	ifEx2();
+	ifEx3();
+}
+	
+
+
+private static void ifEx() {
+	int score= (int)(Math.random()*20)+81;
+	System.out.println("점수= " + score);
+	
+	String grade;
+	
+	if (score>= 90) {
+		if (score>=95) {
+			grade = "A+";
+		} else {
+			grade = "A";
+		} }
+		else {
+		if (score >=85) {
+			grade = "B+";
+		} else {
+			grade = "B";
+		}
+		}
+		System.out.println("학점: " + grade);
+		
+	}
+	
+private static void ifEx2() {
+	int time= (int)(Math.random()*4)+8;
+	System.out.println("현재시간: " + time + "시");
+	
+	switch (time) {
+	case 8: System.out.println("출근합니다");
+	case 9: System.out.println("회의를 합니다");
+	case 10: System.out.println("업무를 봅니다");
+	default: System.out.println("외근 중 입니다");
+}
+}
+private static void ifEx3() {
+	int sum=0;
+	for(int i=1; i<=100; i++) {
+		sum=sum+1;
+	
+	}
+	System.out.println("1~100까지의 합: " + sum);
+}
+}
+
+
--- JavaEx/src/practice/practice01_2.java
+++ JavaEx/src/practice/practice01_2.java
@@ -1,22 +1,111 @@
 package practice;
 
+import java.util.Scanner;
+
+//4장 연습문제
 public class practice01_2 {
 
 	public static void main(String[] args) {
-		guguEx();
-
-	}
-	private static void guguEx() {
+		Ex3();
+		Ex4();
+		Ex5();
+		Ex6();
+		Ex7();
 		
-		//for (제어 변수 초기화;반복조건 체크;증감식)
+		
+	}
 	
-	for (int i=0; i<=6; i++) {
-		for (int a= 1; a<7; a++) {
+	//4장 연습문제 #7
+	private static void Ex7() {
+		boolean run = true; //run while 문 반복
+		int balance = 0;
+		Scanner scanner = new Scanner(System.in);// ctrl+shift+o
+				
+		while (run) {
+			System.out.println("-------------------------------------------");
+			System.out.println("1.예금 | 2. 출금 | 3. 잔고| 4.종료");
+			System.out.println("-------------------------------------------");
+			System.out.print("선택> ");
+			
+			int menuNum = scanner.nextInt();
+			
+			switch(menuNum) {
+				case 1: 
+					System.out.println("예금액> ");
+					balance += scanner.nextInt();
+					break;
+				case 2:
+					System.out.println("출금액> ");
+					balance -=scanner.nextInt();
+					break;
+				case 3: 
+					System.out.println("잔고> ");
+					System.out.println(balance);
+					break;
+				case 4: 
+					run = false;
+					break;
+					
+			}
 			System.out.println();
 		}
-		System.out.println(i * 0);
+		System.out.println("프로그램 종료");
+		
 	}
+	
+	//4장 연습문제 #6
+	private static void Ex6() {
+		for (int i=1; i<=5; i++) {
+			for(int j=1; j<=i;j++) {
+				System.out.print("*");
+				if (j==i) {
+					System.out.println();
+					
+				}
+			}
+		}
+	}
+	
+	//4장 연습문제 #5
+	private static void Ex5() {
+		for (int x=1; x<=10; x++) {
+			for (int y=1; y<=10; y++) {
+				if (4*x+5*y==60) {
+					System.out.println("(" + x + "," +y + ")");
+				}
+			}
 		}
 		
-
+		
+	}
+	
+	//4장 연습문제 #4
+	private static void Ex4() {
+		while (true) {
+			int num1 = (int)(Math.random()*6)+1;
+			int num2= (int)(Math.random()*6)+1;
+			System.out.println("(" + num1 + "," +num2 + ")");
+			
+			if (num1+ num2 ==5) {
+				break;
+			}
+		}
+		
+		
+	}
+	
+	//4장 확인문제 #3
+	private static void Ex3() {
+		int sum=0;
+		
+		 for (int i=1;i<=100;i++) {
+			 
+			 if (i%3==0) {
+				 sum+=i;
+		
+	 }
+	}
+		 System.out.println("3의 배수의 합 : " + sum);
+	}
+	
 }
