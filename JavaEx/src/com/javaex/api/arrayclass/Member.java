02a3d457a08571a36728d00f8e2c963b4f1fa2ee
김소은 <soeunkim0409@gmail.com>
2021-05-10 10:41:44 +0900

    Array 클래스 (정렬, 검색)

---
+++ JavaEx/src/com/javaex/api/arrayclass/Member.java
@@ -0,0 +1,31 @@
+package com.javaex.api.arrayclass;
+
+
+//사용자 정의 객체의 비교를 위해서는 Comparable  인터페이스 구현 필요!
+public class Member implements Comparable {
+	String name;
+	
+	public Member(String name) { 
+		this.name=name;
+		
+	}
+
+	@Override
+	public String toString () {		
+		return "Member [name=" + name +"]";
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		//결과가 0이면 두 객체는 순서가 같음
+		//-1이면(음수) 앞의 객체가 먼저
+		//1이면 (양수) 앞의 객체가 뒤
+		if (o instanceof Member) {
+			Member other= (Member)o;
+			return name.compareTo(other.name);
+		}
+		return 0;
+	}
+
+
+}
