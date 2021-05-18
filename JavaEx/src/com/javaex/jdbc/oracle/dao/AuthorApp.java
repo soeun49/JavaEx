d484ef6cd68e1727df8039d3fe17938a69af695b
김소은 <soeunkim0409@gmail.com>
2021-05-18 12:05:17 +0900

    DAO PATTERN: SELECT,INSERT, UPDATE, DELETE

---
+++ JavaEx/src/com/javaex/jdbc/oracle/dao/AuthorApp.java
@@ -0,0 +1,82 @@
+package com.javaex.jdbc.oracle.dao;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class AuthorApp {
+
+	public static void main(String[] args) {
+		//selectAll();
+		//insertAuthor();
+		//updateAuthor();
+		deleteAuthor();
+	}
+	private static void deleteAuthor() {
+		//Scanner에서 저자 pk 입력 -> delete
+		Scanner scanner = new Scanner (System.in);
+		selectAll();
+		System.out.print("삭제할 저자:");
+		int id = scanner.nextInt();
+
+		AuthorDAO dao = new AuthorDAOImpl();
+		boolean success = dao.delete(Long.valueOf(id));
+	
+		System.out.println("DELETE " + (success ? "성공": "실패"));
+		selectAll();
+		scanner.close();
+	}
+	private static void updateAuthor() {
+		//scanner에서 저자 pk, 저자 설명을 입력 -> update
+		Scanner scanner = new Scanner(System.in);
+		selectAll();
+		System.out.print("수정할 저자: ");
+		int id = scanner.nextInt();
+		System.out.print("수정할 저자 정보:");
+		String desc= scanner.next();
+		
+		AuthorVO vo = new AuthorVO(Long.valueOf(id), null, desc);
+		
+		AuthorDAO dao = new AuthorDAOImpl();
+				
+		boolean success = dao.update(vo);
+		
+		System.out.println("UPDATE " + (success ? "성공": "실패"));
+		
+		selectAll();
+		
+	}
+	
+	private static void insertAuthor() {
+		//scanner에서 이름 입력 -> insert
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("저자 이름: ");
+		String name = scanner.next();
+		
+		AuthorVO vo = new AuthorVO(null,name);
+		AuthorDAO dao = new AuthorDAOImpl ();
+		
+		boolean success = dao.insert(vo);
+		
+		System.out.println("INSERT " + (success ?  "성공": "실패"));
+		
+		selectAll();
+		scanner.close();
+	}
+	private static void selectAll() {
+		AuthorDAO dao= new AuthorDAOImpl();
+		List<AuthorVO> list = dao.getList();
+		
+		// Iterator
+		Iterator <AuthorVO> it= list.iterator();
+		
+		System.out.println("===============Author List");
+		
+		while (it.hasNext()) {
+			AuthorVO item= it.next(); // iterator에서 요소 추출
+			System.out.printf ("%d\t%s\t%s%n",item.getId(), item.getAuthorName(), item.getAuthorDesc());
+			
+		}
+	}
+}
